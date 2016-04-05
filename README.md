825e12ed23ab41d5fdfd573df5dc218e31f0f4c9
kbabshet <kbabshet@hotmail.com>
2016-04-05 16:06:10 +0200

    unique together and input types defined

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Smart-Power-IoT
 ELEN4009 Smart Home Power Management (Project 18) - Ari Croock, Kanaka Babshet. Alice Yang, Daniel Weinberg
+'smart_home' contains the project
+'devices' contains the app that manages devices
+'manage.py' is the interface for the project
--- smart_home/devices/models.py
+++ smart_home/devices/models.py
@@ -7,17 +7,18 @@ class DeviceState(models.Model):
     def __str__(self):
         return self.state
 
-
 class Device(models.Model):
     device_name = models.CharField(max_length=200)
-    serial_num = models.CharField(max_length=200)
-    ip_addr = models.CharField(max_length=200)
+    serial_num = models.IntegerField()
+    ip_addr = models.GenericIPAddressField()
     location = models.ForeignKey('Location', on_delete=models.CASCADE)
     current_state = models.ForeignKey('DeviceState', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.device_name
 
+    class Meta:
+        unique_together = (('device_name', 'location'),('device_name','ip_addr'))
 
 class Location(models.Model):
     location_name = models.CharField(max_length=200)
