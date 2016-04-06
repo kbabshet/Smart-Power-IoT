939eaea9591f9bbcf9c5f5b0585173ccd57e8552
Ari Croock <aricroock@gmail.com>
2016-04-06 15:10:37 +0200

    Fix checkbox state when page is loaded

--- smart_home/devices/views.py
+++ smart_home/devices/views.py
@@ -7,22 +7,26 @@ from django.template import RequestContext, loader
 from .models import Device
 from .models import DeviceState
 
-#def index(request):
+# def index(request):
 #    device_list = Device.objects.order_by('device_name')[:10]
 #    response = "\n".join([device.device_name for device in device_list])
 #    return HttpResponse(response)
 
+
 def home_page(request):
     device_list = Device.objects.order_by('device_name')[:10]
-    return render(request,"index.html", {'devices': device_list})
+    return render(request, "index.html", {'devices': device_list})
+
 
 def list(request):
     device_list = Device.objects.order_by('device_name')[:10]
-    return render(request,"device.html", {'devices': device_list})
+    return render(request, "device.html", {'devices': device_list})
+
 
 def detail(request, device_id):
-	device = get_object_or_404(Device, pk=device_id)
-	return render(request, "detail.html", {'device': device})
+    device = get_object_or_404(Device, pk=device_id)
+    return render(request, "detail.html", {'device': device})
+
 
 @csrf_exempt
 def change_device_state(request, device_id):
