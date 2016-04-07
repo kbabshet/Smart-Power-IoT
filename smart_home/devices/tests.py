8d433ccbd374a1901b9528935fd929c3fa5b93aa
aliceyang16 <yangalice8@gmail.com>
2016-04-07 14:14:04 +0200

    Front End test implementation in test.py

--- smart_home/devices/tests.py
+++ smart_home/devices/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from .models import Device, Location, DeviceState
 from django.db import IntegrityError
+from django.test import Client
+from django.core.urlresolvers import reverse
 
 class uniqueMethodTests(TestCase):
 
@@ -67,3 +69,27 @@ class uniqueMethodTests(TestCase):
 
         with self.assertRaises(IntegrityError):
             device2.save()
+
+class viewTests(TestCase):
+    def test_index_view_page(self):
+        response = self.client.get(reverse('devices:home_page'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_view_page(self):
+        kitchen = Location(location_name='kitchen').save()
+        device1 = Device(device_name='kettle',serial_num='12345',ip_addr='172.16.254.1',
+                         location=Location.objects.get(location_name='kitchen'),
+                         current_state=DeviceState.objects.get(state='on'))
+
+        device1.save()
+
+        response = self.client.get(reverse('devices:detail', kwargs={'device_id': 1}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_view_page(self):
+        response = self.client.get(reverse('devices:list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_view_page(self):
+        response = self.client.get(reverse('devices:login'))
+        self.assertEqual(response.status_code, 200)
